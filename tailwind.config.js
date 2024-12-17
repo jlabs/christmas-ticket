bfc2e386d204606364acaa845842e224aa75db6a
J <adams.j.6385@gmail.com>
2024-12-17 22:19:54 +0000

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        christmas: {
+          red: "#A50000",
+          green: "#008000",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
